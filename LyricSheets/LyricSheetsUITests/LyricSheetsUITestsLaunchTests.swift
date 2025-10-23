418cf9e02fba1cad1cf876544be28de5bc20b97d
Palmer Nix <nix.palmer9200@gmail.com>
2025-10-23 16:05:51 -0400

    Create XCode project and move existing iOS app (swift) files into new LyricSheets XCode project folder

---
+++ LyricSheets/LyricSheetsUITests/LyricSheetsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LyricSheetsUITestsLaunchTests.swift
+//  LyricSheetsUITests
+//
+//  Created by Palmer Nix on 10/23/25.
+//
+
+import XCTest
+
+final class LyricSheetsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
